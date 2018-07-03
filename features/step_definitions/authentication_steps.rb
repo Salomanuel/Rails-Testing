c700671317c78a15dd5f8c1ccb43af33b2d1903e
Salomanuel <salomanuel.aux+code@gmail.com>
2018-07-03 16:25:12 +0200

    finish filling the authentication steps specs

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -31,21 +31,29 @@ Given("I am a registered user") do
 end
 
 When("I fill in the login form") do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in "user_email", :with => "tester@testdomain.test"
+  fill_in "user_password", :with => "pa$$word"
+
+  click_button "Log in"
 end
 
 Then("I should be logged in") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content("Logged in")
 end
 
 Given("I am logged in") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit root_path
+
+  fill_in "user_email", :with => "tester@testdomain.test"
+  fill_in "user_password", :with => "pa$$word"
+
+  click_button "Log in"
 end
 
 When("I click on the log out button") do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_button "Log out"
 end
 
 Then("I should be redirected to the log in page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content("Log in")
 end
