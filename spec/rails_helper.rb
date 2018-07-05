0341ce21a065c522a9afb04b851276ecc56e634c
Salomanuel <salomanuel.aux+code@gmail.com>
2018-07-05 16:08:09 +0200

    set up controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,4 +78,12 @@ RSpec.configure do |config|
       end
     end
   end
+
+  require "support/controller/helpers"
+  RSpec.configure do |config|
+
+    config.include Warden::Test::Helpers
+    config.include Devise::TestHelpers, type: :controller
+    config.include ControllerHelpers, type: :controller
+  end
 end
