4e57b448034646263086cb0cb32b37abe31ec5cd
Salomanuel <salomanuel.aux+code@gmail.com>
2018-07-05 13:13:19 +0200

    add several tests for destroy

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -93,5 +93,24 @@ RSpec.describe BooksController, :type => :controller do
         expect(response).to render_template(:edit)
       end
     end
+
+    describe "DELETE #destroy" do
+      let(:book) { FactoryBot.build_stubbed(:book) }
+
+      before do
+        allow(Book).to receive(:find).and_return(book)
+        allow(book).to receive(:destroy)
+        
+        delete :destroy, :params => { :id => book.id }
+      end
+
+      it "deletes the book" do
+        expect(book).to have_received(:destroy)
+      end
+
+      it "redirects to the index page" do
+        expect(response).to redirect_to(books_path)
+      end
+    end
   end
 end
