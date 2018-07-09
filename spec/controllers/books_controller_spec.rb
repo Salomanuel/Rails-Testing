c02412d98acfb66209ff7d6a15c259d1c8f2a659
Salomanuel <salomanuel.aux+code@gmail.com>
2018-07-09 15:10:28 +0200

    add tests for updating books

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -64,4 +64,35 @@ RSpec.describe BooksController do
       end
     end
   end
+
+  describe "PATCH #update" do
+    let(:book) { FactoryBot.build_stubbed(:book) }
+
+    before do
+      allow(Book).to receive(:find).and_return(book)
+      allow(book).to receive(:update).and_return(true)
+    end
+
+    it "updates the book" do
+      patch :update, params: { 
+        id: book.id,
+        book: { name: "New Name" } }
+
+      expect(book).to have_received(:update)
+    end
+
+    context "when the update fails" do
+      before do
+        allow(book).to receive(:update).and_return(false)
+      end
+
+      it "renders the edit page again" do
+        patch :update, params: {
+          id: book.id,
+          book: { name: "New Name" } }
+
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
 end
