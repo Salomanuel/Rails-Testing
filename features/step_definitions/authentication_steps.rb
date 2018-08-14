69e59cdc84ca0d663d78455044c752c2813bdad6
Manuel Salomone <salomanuel.aux+code@gmail.com>
2018-08-14 10:20:05 +0200

    write 6 more steps

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -12,27 +12,46 @@ When("I fill in the sign up form") do
 end
 
 When("I confirm the email") do
-  pending # Write code here that turns the phrase above into concrete actions
+  open_mail("tester@testdomain.test")
+  
+  visit_in_email("Confirm my account")
 end
 
 Then("I should see that my account is confirmed") do
-  pending # Write code here that turns the phrase above into concrete actions
+  message = "Your email address has been successfully confirmed"
+
+  expect(page).to have_content(message)
 end
 
 Given("I am a registered user") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @registered_user = FactoryBot.create(:user,
+                                       email: "tester@testdomain.test",
+                                       password: "pa$$word")
 end
 
 When("I fill in the login form") do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in "user_email", with: "tester@testdomain.test"
+  fill_in "user_password", with: "pa$$word"
+
+  click_button "Log in"
 end
 
 Then("I should be logged in") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit root_path
+
+  fill_in "user_email", with: "tester@testdomain.test"
+  fill_in "user_password", with: "pa$$word"
+
+  click_button "Log in"
 end
 
 Given("I am logged in") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit root_path
+
+  fill_in "user_email", with: "tester@testdomain.test"
+  fill_in "user_password", with: "pa$$word"
+
+  click_button "Log in"
 end
 
 When("I click on the log out button") do
